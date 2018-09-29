c5ecacc3749b6c3e9f340ee3c1eb86c0f5b8f6a7
Ben Hale <bhale@pivotal.io>
2018-09-29 11:28:51 -0700

    Initial CI Updates

---
+++ src/main/java/io/r2dbc/h2/H2ConnectionFactory.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.r2dbc.h2;
+
+import io.r2dbc.spi.Connection;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryMetadata;
+import org.reactivestreams.Publisher;
+
+/**
+ * An implementation of {@link ConnectionFactory} for creating connections to an H2 database.
+ */
+
+public final class H2ConnectionFactory implements ConnectionFactory {
+
+    @Override
+    public Publisher<? extends Connection> create() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public ConnectionFactoryMetadata getMetadata() {
+        // TODO
+        return null;
+    }
+}
