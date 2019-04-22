f9b5d15a35503249be8dce190c502f69c3472552
pull-vert <frederic.montariol@gmail.com>
2019-04-22 10:20:29 +0200

    add UuidCodec

---
+++ src/main/java/io/r2dbc/h2/codecs/UuidCodec.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.r2dbc.h2.codecs;
+
+import io.r2dbc.h2.util.Assert;
+import org.h2.value.Value;
+import org.h2.value.ValueUuid;
+
+import java.util.UUID;
+
+/**
+ * @author Fred Montariol
+ */
+final class UuidCodec extends AbstractCodec<UUID> {
+
+    UuidCodec() {
+        super(UUID.class);
+    }
+
+    @Override
+    boolean doCanDecode(int dataType) {
+        return Value.UUID == dataType;
+    }
+
+    @Override
+    UUID doDecode(Value value, Class<? extends UUID> type) {
+        return (UUID) value.convertTo(Value.UUID).getObject();
+    }
+
+    @Override
+    Value doEncode(UUID value) {
+        return ValueUuid.get(Assert.requireNonNull(value, "value must not be null"));
+    }
+}
---
+++ src/test/java/io/r2dbc/h2/codecs/UuidCodecTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.r2dbc.h2.codecs;
+
+import org.h2.value.Value;
+import org.h2.value.ValueNull;
+import org.h2.value.ValueUuid;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+final class UuidCodecTest {
+
+    @Test
+    void decode() {
+        assertThat(new UuidCodec().decode(ValueUuid.get("79e9eb45-2835-49c8-ad3b-c951b591bc7f"), UUID.class))
+            .isEqualTo(UUID.fromString("79e9eb45-2835-49c8-ad3b-c951b591bc7f"));
+    }
+
+    @Test
+    void doCanDecode() {
+        UuidCodec codec = new UuidCodec();
+
+        assertThat(codec.doCanDecode(Value.UUID)).isTrue();
+        assertThat(codec.doCanDecode(Value.UNKNOWN)).isFalse();
+        assertThat(codec.doCanDecode(Value.INT)).isFalse();
+    }
+
+    @Test
+    void doEncode() {
+        assertThat(new UuidCodec().doEncode(UUID.fromString("79e9eb45-2835-49c8-ad3b-c951b591bc7f")))
+            .isEqualTo(ValueUuid.get("79e9eb45-2835-49c8-ad3b-c951b591bc7f"));
+    }
+
+    @Test
+    void doEncodeNoValue() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new UuidCodec().doEncode(null))
+            .withMessage("value must not be null");
+    }
+
+    @Test
+    void encodeNull() {
+        assertThat(new UuidCodec().encodeNull())
+            .isEqualTo(ValueNull.INSTANCE);
+    }
+}
