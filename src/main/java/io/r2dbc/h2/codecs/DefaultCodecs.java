f7559b21823a42dabba7c55adcf6cd8e2149bc52
Michael Simons <michael@simons.ac>
2019-09-23 18:21:19 +0200

    Remove GeometryCodec from default codec list.

--- src/main/java/io/r2dbc/h2/codecs/DefaultCodecs.java
+++ src/main/java/io/r2dbc/h2/codecs/DefaultCodecs.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,6 @@ public final class DefaultCodecs implements Codecs {
                 new ClobCodec(client),
                 new DoubleCodec(),
                 new FloatCodec(),
-                new GeometryCodec(),
                 new IntegerCodec(),
                 new LocalDateCodec(),
                 new LocalDateTimeCodec(),
