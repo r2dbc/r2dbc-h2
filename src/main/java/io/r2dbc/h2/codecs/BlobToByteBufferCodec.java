5f02ed284ea521a96c961474400bfc958e6cee33
Mark Paluch <mpaluch@pivotal.io>
2019-12-02 15:43:58 +0100

    Polishing
    Convert tabs to spaces.
    
    [#127]

--- src/main/java/io/r2dbc/h2/codecs/BlobToByteBufferCodec.java
+++ src/main/java/io/r2dbc/h2/codecs/BlobToByteBufferCodec.java
@@ -16,12 +16,6 @@
 
 package io.r2dbc.h2.codecs;
 
-import java.io.InputStream;
-import java.io.SequenceInputStream;
-import java.nio.ByteBuffer;
-import java.util.Enumeration;
-import java.util.Iterator;
-
 import io.r2dbc.h2.client.Client;
 import io.r2dbc.h2.util.Assert;
 import io.r2dbc.spi.Blob;
@@ -30,73 +24,79 @@ import org.h2.value.ValueNull;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.nio.ByteBuffer;
+import java.util.Enumeration;
+import java.util.Iterator;
+
 final class BlobToByteBufferCodec extends AbstractCodec<ByteBuffer> {
 
-	private final Client client;
+    private final Client client;
 
-	BlobToByteBufferCodec(Client client) {
-		super(ByteBuffer.class);
-		this.client = client;
-	}
+    BlobToByteBufferCodec(Client client) {
+        super(ByteBuffer.class);
+        this.client = client;
+    }
 
-	@Override
-	boolean doCanDecode(int dataType) {
-		return dataType == Value.BLOB;
-	}
+    @Override
+    boolean doCanDecode(int dataType) {
+        return dataType == Value.BLOB;
+    }
 
-	@Override
-	ByteBuffer doDecode(Value value, Class<? extends ByteBuffer> type) {
-		if (value == null || value instanceof ValueNull) {
-			return null;
-		}
+    @Override
+    ByteBuffer doDecode(Value value, Class<? extends ByteBuffer> type) {
+        if (value == null || value instanceof ValueNull) {
+            return null;
+        }
 
-		return new ValueLobBlob(value).stream()
-			.reduce((byteBuffer, byteBuffer2) -> {
-				ByteBuffer combined = ByteBuffer.allocate(byteBuffer.limit() + byteBuffer2.limit());
-				combined.put(byteBuffer);
-				combined.put(byteBuffer2);
-				return combined;
-			})
-			.block();
-	}
+        return new ValueLobBlob(value).stream()
+            .reduce((byteBuffer, byteBuffer2) -> {
+                ByteBuffer combined = ByteBuffer.allocate(byteBuffer.limit() + byteBuffer2.limit());
+                combined.put(byteBuffer);
+                combined.put(byteBuffer2);
+                return combined;
+            })
+            .block();
+    }
 
-	@Override
-	Value doEncode(ByteBuffer value) {
-		Assert.requireNonNull(value, "value must not be null");
+    @Override
+    Value doEncode(ByteBuffer value) {
+        Assert.requireNonNull(value, "value must not be null");
 
-		Value blob = this.client.getSession().getDataHandler().getLobStorage().createBlob(
-			new SequenceInputStream(
-				new BlobInputStreamEnumeration(value)), -1);
+        Value blob = this.client.getSession().getDataHandler().getLobStorage().createBlob(
+            new SequenceInputStream(
+                new BlobInputStreamEnumeration(value)), -1);
 
-		this.client.getSession().addTemporaryLob(blob);
+        this.client.getSession().addTemporaryLob(blob);
 
-		return blob;
-	}
+        return blob;
+    }
 
-	/**
-	 * Converts a {@link Flux} of {@link Blob}s into an {@link Enumeration} of {@link InputStream}s.
-	 */
-	private final class BlobInputStreamEnumeration implements Enumeration<InputStream> {
+    /**
+     * Converts a {@link Flux} of {@link Blob}s into an {@link Enumeration} of {@link InputStream}s.
+     */
+    private final class BlobInputStreamEnumeration implements Enumeration<InputStream> {
 
-		private final Iterator<ByteBufferInputStream> inputStreams;
+        private final Iterator<ByteBufferInputStream> inputStreams;
 
-		BlobInputStreamEnumeration(ByteBuffer value) {
-			this.inputStreams = Flux.just(value)
-				.map(ByteBufferInputStream::new)
-				.subscribeOn(Schedulers.elastic())
-				.cancelOn(Schedulers.elastic())
-				.toIterable()
-				.iterator();
-		}
+        BlobInputStreamEnumeration(ByteBuffer value) {
+            this.inputStreams = Flux.just(value)
+                .map(ByteBufferInputStream::new)
+                .subscribeOn(Schedulers.elastic())
+                .cancelOn(Schedulers.elastic())
+                .toIterable()
+                .iterator();
+        }
 
-		@Override
-		public boolean hasMoreElements() {
-			return inputStreams.hasNext();
-		}
+        @Override
+        public boolean hasMoreElements() {
+            return inputStreams.hasNext();
+        }
 
-		@Override
-		public InputStream nextElement() {
-			return inputStreams.next();
-		}
-	}
+        @Override
+        public InputStream nextElement() {
+            return inputStreams.next();
+        }
+    }
 }
