8626b40d3d9be06b08546525e83efaa052d21998
Greg Turnquist <gturnquist@pivotal.io>
2019-12-16 14:55:25 -0600

    Polishing.

--- ci/test.sh
+++ ci/test.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/spring-hateoas-maven-repository" ./mvnw -P${PROFILE} clean dependency:list test -Dsort -B
+MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/r2dbc-maven-repository" ./mvnw -P${PROFILE} clean dependency:list test -Dsort -B
